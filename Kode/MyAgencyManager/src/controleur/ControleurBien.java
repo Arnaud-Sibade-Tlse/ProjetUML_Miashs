d5e73ce068dcd4d4c2aa26533a1537cd0f8086d1
DarkVasDehors <sibade.arnaud@gmail.com>
2017-12-20 17:03:51 +0100

    ControleurBien en cours

--- Kode/MyAgencyManager/src/controleur/ControleurBien.java
+++ Kode/MyAgencyManager/src/controleur/ControleurBien.java
@@ -1,7 +1,54 @@
 package controleur;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class ControleurBien {
+	
+	Scanner sc = new Scanner(System.in);
+
 	public void verifBien(){
 		//TODO
+	}
+	
+	public void creerBien(){
+		System.out.println("__Creation d'un Bien");
+		System.out.print("Bien Maison, Appartement ou Terrain ? (M/A/T) ? ");
+		
+		String type = "";
+		do{
+			try{
+				type = sc.next();
+			}catch(Exception e){
+				type = "";
+			}
+		}while(type.equals("A") || type.equals("M") || type.equals("T"));
+		
+		System.out.println();
+		
+		if(type.equals("M")){
+			System.out.println("____Creation d'une Maison");
+		}
+		else if(type.equals("A")){
+			System.out.println("____Creation d'un Appartement");
+		} else {
+			System.out.println("____Creation d'un Terrain");
+
+		}
+		
+		String nom;
+		System.out.print("Nom :");
+		do{
+			try{
+				nom = sc.next();
+			}catch(Exception e){
+				nom = "";
+			}
+		}while(!Pattern.matches("^[a-zA-Z ]+$", nom));
+		
+		System.out.println("ok");
+		String adresse;
+		String numTel;
+		String mail;
 	}
 }
